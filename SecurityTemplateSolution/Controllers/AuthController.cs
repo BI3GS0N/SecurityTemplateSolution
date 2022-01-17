7468a20cfff81c1e79a0097b34d42e14b791434d
Sylwester Biega <biegson11@gmail.com>
2022-01-17 22:48:00 +0100

    Add AuthController, add AuthRequest class

---
+++ SecurityTemplateSolution/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SecurityTemplateSolution.Models;
+
+namespace SecurityTemplateSolution.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly SecurityDbContext _context;
+        public AuthController(SecurityDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Register([FromBody]AuthRequest request)
+        {
+            var user = new User
+            {
+                Login = request.Login
+            };
+
+            _context.Add(user);
+            _context.SaveChanges();
+            return Ok(new { user.Id, user.Login });
+        }
+       
+    }
+}
