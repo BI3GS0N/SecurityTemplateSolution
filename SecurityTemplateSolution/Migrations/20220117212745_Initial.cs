e48c5fe216914949d2f84c61419c73e38e898035
Sylwester Biega <biegson11@gmail.com>
2022-01-17 22:30:04 +0100

    Add User Model, DbContext, Migration

---
+++ SecurityTemplateSolution/Migrations/20220117212745_Initial.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SecurityTemplateSolution.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Login = table.Column<string>(type: "nvarchar(60)", nullable: false),
+                    PasswordHash = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    PasswordSalt = table.Column<byte[]>(type: "varbinary(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Login",
+                table: "Users",
+                column: "Login",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ SecurityTemplateSolution/Models/SecurityDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SecurityTemplateSolution.Models
+{
+    public class SecurityDbContext : DbContext
+    {
+        public SecurityDbContext(DbContextOptions<SecurityDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelbuilder) 
+        {
+            modelbuilder.Entity<User>()
+                .HasIndex(p=>p.Login)
+                .IsUnique(true);
+        }
+    }
+}
