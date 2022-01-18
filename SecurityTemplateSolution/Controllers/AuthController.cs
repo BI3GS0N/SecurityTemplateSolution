9c0557da2532a453c51d7775e82ba1f1ee42e230
Sylwester Biega <biegson11@gmail.com>
2022-01-18 19:57:35 +0100

    TokenSettings DI in Controller, Add Login method, add verify password in auth service, login works

--- SecurityTemplateSolution/Controllers/AuthController.cs
+++ SecurityTemplateSolution/Controllers/AuthController.cs
@@ -1,8 +1,14 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using SecurityTemplateSolution.Models;
 using SecurityTemplateSolution.Services;
+using System;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 
 namespace SecurityTemplateSolution.Controllers
 {
@@ -12,9 +18,12 @@ namespace SecurityTemplateSolution.Controllers
     public class AuthController : ControllerBase
     {
         private readonly SecurityDbContext _context;
-        public AuthController(SecurityDbContext context)
+        private readonly TokenSettings _tokenSettings;
+
+        public AuthController(SecurityDbContext context, IOptions<TokenSettings> tokenSettings)
         {
             _context = context;
+            _tokenSettings = tokenSettings.Value;
         }
 
         [AllowAnonymous]
@@ -33,6 +42,36 @@ namespace SecurityTemplateSolution.Controllers
             _context.Add(user);
             _context.SaveChanges();
             return Ok(new { user.Id, user.Login });
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] AuthRequest request)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Login == request.Login);
+            if (user == null)
+                BadRequest("Nieprawidłowa nazwa użytkownika");
+
+            if (AuthService.VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+            {
+                BadRequest("Nieprawidłowe hasło");
+            }
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_tokenSettings.SecretKey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddHours(6),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return Ok(new { Token = tokenString });
         }
 
         [HttpGet("users")]
--- SecurityTemplateSolution/Startup.cs
+++ SecurityTemplateSolution/Startup.cs
@@ -65,8 +65,6 @@ namespace SecurityTemplateSolution
                         ValidateAudience = false
                     };
                 });
-
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
