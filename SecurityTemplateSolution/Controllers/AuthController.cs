588d75a79e4b793b3d8a60b69772734aae816ddb
Sylwester Biega <biegson11@gmail.com>
2022-01-18 18:48:19 +0100

    Configure Auth in controller, add get users method

--- SecurityTemplateSolution/Controllers/AuthController.cs
+++ SecurityTemplateSolution/Controllers/AuthController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using SecurityTemplateSolution.Models;
 using SecurityTemplateSolution.Services;
+using System.Linq;
 
 namespace SecurityTemplateSolution.Controllers
 {
+    [Authorize]
     [Route("api/auth")]
     [ApiController]
     public class AuthController : ControllerBase
@@ -14,6 +17,7 @@ namespace SecurityTemplateSolution.Controllers
             _context = context;
         }
 
+        [AllowAnonymous]
         [HttpPost("register")]
         public IActionResult Register([FromBody]AuthRequest request)
         {
@@ -29,6 +33,13 @@ namespace SecurityTemplateSolution.Controllers
             _context.Add(user);
             _context.SaveChanges();
             return Ok(new { user.Id, user.Login });
+        }
+
+        [HttpGet("users")]
+        public IActionResult GetUsers()
+        {
+            var users =  _context.Users.Select(u => new {u.Id, u.Login }).ToList();
+            return Ok(users);
         }
        
     }
